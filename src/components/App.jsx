4a8d52bc249cbf239c9c01e68de0454457c1dd0d
Naumetsdmytro <113528509+Naumetsdmytro@users.noreply.github.com>
2023-03-13 00:10:21 +0200

    write on hook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,33 +1,70 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Toaster } from 'react-hot-toast';
+import { toast } from 'react-hot-toast';
 
 import { Searchbar } from 'components/Searchbar';
 import { ImageGallery } from 'components/ImageGallery';
+import { Button } from 'components/Button';
+import { fetchImagesByTheme } from '../services/pixabayAPI';
+import { Loader } from 'components/Loader';
 
 import styles from './App.module.css';
 
-export class App extends Component {
-  state = {
-    theme: '',
+export const App = () => {
+  const [theme, setTheme] = useState('');
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [page, setPage] = useState(1);
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    if (!theme) {
+      return;
+    }
+    setLoading(true);
+    const fetchImages = async () => {
+      try {
+        const images = await fetchImagesByTheme(theme, page);
+
+        if (images.hits.length === 0) {
+          toast.error('Please, enter a valid theme');
+        }
+
+        if (page > 1) {
+          setImages(prevstate => [...prevstate, ...images.hits]);
+          return;
+        }
+        setImages(images.hits);
+        setTotal(images.total);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchImages();
+  }, [page, theme]);
+
+  const handleSubmit = theme => {
+    setImages([]);
+    setPage(1);
+    setTheme(theme);
   };
 
-  handleSubmit = theme => {
-    this.setState({ theme });
+  const handleClickButton = () => {
+    setPage(prevstate => prevstate + 1);
+    setTotal(prevstate => prevstate - 12);
   };
 
-  render() {
-    return (
-      <div
-        className={styles.app}
-        style={{
-          fontSize: 40,
-          color: '#010101',
-        }}
-      >
-        <Toaster position="top-right" toastOptions={{ duration: 2500 }} />
-        <Searchbar onSubmit={this.handleSubmit} />
-        <ImageGallery theme={this.state.theme} />
-      </div>
-    );
-  }
-}
+  return (
+    <div className={styles.app}>
+      <Toaster position="top-right" toastOptions={{ duration: 2500 }} />
+      <Searchbar onSubmit={handleSubmit} />
+      {error && <h3>{error.message}</h3>}
+      <ImageGallery images={images} />
+      {loading && <Loader />}
+      {total > 12 && !loading && <Button onClick={handleClickButton} />}
+    </div>
+  );
+};
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,81 +1,23 @@
-import React, { Component } from 'react';
-import { toast } from 'react-hot-toast';
 import PropTypes from 'prop-types';
 
-import { fetchImagesByTheme } from '../../services/pixabayAPI';
 import { ImageGalleryItem } from 'components/ImageGalleryItem';
-import { Loader } from 'components/Loader';
-import { Button } from 'components/Button';
 
 import styles from './ImageGallery.module.css';
 
-export class ImageGallery extends Component {
-  state = {
-    images: [],
-    loading: false,
-    error: false,
-    page: 1,
-    total: 0,
-  };
-
-  async componentDidUpdate(prevprops, prevstate) {
-    if (
-      prevstate.page !== this.state.page ||
-      prevprops.theme !== this.props.theme
-    ) {
-      this.setState({ loading: true });
-      if (prevprops.theme !== this.props.theme) {
-        this.setState({ images: [] });
-      }
-      try {
-        const images = await fetchImagesByTheme(
-          this.props.theme,
-          this.state.page
-        );
-        if (images.hits.length === 0) {
-          toast.error('Please, enter a valid theme');
-        }
-        if (prevprops.theme === this.props.theme && this.state.page !== 1) {
-          this.setState(prev => ({
-            images: [...prev.images, ...images.hits],
-          }));
-          return;
-        }
-        this.setState({ images: images.hits, total: images.total, page: 1 });
-      } catch (error) {
-        this.setState({ error });
-      } finally {
-        this.setState({ loading: false });
-      }
-    }
-  }
-
-  handleClickButton = () => {
-    this.setState(prevstate => ({
-      page: prevstate.page + 1,
-      total: prevstate.total - 12,
-    }));
-  };
-
-  render() {
-    const { images, error, loading, total } = this.state;
-    return (
-      <>
-        {error && <h3>{error.message}</h3>}
-        {images.length > 0 && (
-          <ul className={styles.gallery}>
-            {images.map(image => (
-              <ImageGalleryItem image={image} key={image.id} />
-            ))}
-          </ul>
-        )}
-        {loading && <Loader />}
-        {total > 12 && !loading && <Button onClick={this.handleClickButton} />}
-      </>
-    );
-  }
-}
+export const ImageGallery = ({ images }) => {
+  return (
+    <>
+      {images.length > 0 && (
+        <ul className={styles.gallery}>
+          {images.map(image => (
+            <ImageGalleryItem image={image} key={image.id} />
+          ))}
+        </ul>
+      )}
+    </>
+  );
+};
 
 ImageGallery.propTypes = {
-  theme: PropTypes.string.isRequired,
+  images: PropTypes.array.isRequired,
 };
